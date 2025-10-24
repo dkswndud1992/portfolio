8d44f5631df70303029266a77cf18021c1e64ed7
jyahn0302 <jyahn0302@gmail.com>
2025-10-24 07:19:29 +0000

    feat: header component 수정

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
