ae12b322916946a51f4c5bc875414f663f46ed4e
jyahn0302 <jyahn0302@gmail.com>
2025-11-07 13:43:20 +0000

    feat: Update skills section with new technologies and categories
    Refactor: Rename 'Others & Robotics' category to 'etc' in skills.html
    Update: Backend skills to include Spring, Node.js, and Flask
    Update: Frontend skills to include Flutter, Vue.js, and React.js
    Update: 'etc' skills to include ROS, Kubernetes, PostgreSQL, Docker, and Git

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -16,26 +16,22 @@ interface Skill {
 })
 export class SkillsComponent {
   backendSkills: Skill[] = [
-    { name: 'Java', badgeUrl: 'https://img.shields.io/badge/Java-007396?style=flat&logo=java&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Java_(programming_language)' },
     { name: 'Spring', badgeUrl: 'https://img.shields.io/badge/Spring-6DB33F?style=flat&logo=spring&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Spring_Framework' },
-    { name: 'Python', badgeUrl: 'https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Python_(programming_language)' },
-    { name: 'Go', badgeUrl: 'https://img.shields.io/badge/Go-00ADD8?style=flat&logo=go&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Go_(programming_language)' }
+    { name: 'Node.js', badgeUrl: 'https://img.shields.io/badge/Node.js-339933?style=flat&logo=nodedotjs&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Node.js' },
+    { name: 'Flask', badgeUrl: 'https://img.shields.io/badge/Flask-000000?style=flat&logo=flask&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Flask_(web_framework)' }
   ];
 
   frontendSkills: Skill[] = [
-    { name: 'JavaScript', badgeUrl: 'https://img.shields.io/badge/JavaScript-F7DF1E?style=flat&logo=javascript&logoColor=black', wikiUrl: 'https://en.wikipedia.org/wiki/JavaScript' },
-    { name: 'TypeScript', badgeUrl: 'https://img.shields.io/badge/TypeScript-3178C6?style=flat&logo=typescript&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/TypeScript' },
-    { name: 'HTML5', badgeUrl: 'https://img.shields.io/badge/HTML5-E34F26?style=flat&logo=html5&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/HTML5' },
-    { name: 'CSS3', badgeUrl: 'https://img.shields.io/badge/CSS3-1572B6?style=flat&logo=css3&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/CSS' },
-    { name: 'Angular', badgeUrl: 'https://img.shields.io/badge/Angular-DD0031?style=flat&logo=angular&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Angular_(web_framework)' },
-    { name: 'Flutter', badgeUrl: 'https://img.shields.io/badge/Flutter-02569B?style=flat&logo=flutter&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Flutter' }
+    { name: 'Flutter', badgeUrl: 'https://img.shields.io/badge/Flutter-02569B?style=flat&logo=flutter&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Flutter' },
+    { name: 'Vue.js', badgeUrl: 'https://img.shields.io/badge/Vue.js-4FC08D?style=flat&logo=vuedotjs&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Vue.js' },
+    { name: 'React.js', badgeUrl: 'https://img.shields.io/badge/React-61DAFB?style=flat&logo=react&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/React_(software)' }
   ];
 
   othersAndRoboticsSkills: Skill[] = [
     { name: 'ROS', badgeUrl: 'https://img.shields.io/badge/ROS-22314E?style=flat&logo=ros&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Robot_Operating_System' },
-    { name: 'Docker', badgeUrl: 'https://img.shields.io/badge/Docker-2496ED?style=flat&logo=docker&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Docker_(software)' },
-    { name: 'Oracle', badgeUrl: 'https://img.shields.io/badge/Oracle-F80000?style=flat&logo=oracle&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Oracle_Database' },
+    { name: 'Kubernetes', badgeUrl: 'https://img.shields.io/badge/Kubernetes-326CE5?style=flat&logo=kubernetes&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Kubernetes' },
     { name: 'PostgreSQL', badgeUrl: 'https://img.shields.io/badge/PostgreSQL-336791?style=flat&logo=postgresql&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/PostgreSQL' },
-    { name: 'MySQL', badgeUrl: 'https://img.shields.io/badge/MySQL-4479A1?style=flat&logo=mysql&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/MySQL' }
+    { name: 'Docker', badgeUrl: 'https://img.shields.io/badge/Docker-2496ED?style=flat&logo=docker&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Docker_(software)' },
+    { name: 'Git', badgeUrl: 'https://img.shields.io/badge/Git-F05032?style=flat&logo=git&logoColor=white', wikiUrl: 'https://en.wikipedia.org/wiki/Git' }
   ];
 }
